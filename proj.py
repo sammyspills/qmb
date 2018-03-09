4a8dbe444276ca9e5af22c304587a1849f9b7824
sammyspills <py14sts@leeds.ac.uk>
2018-03-09 16:19:40 +0000

    Temp 09.03.18

--- proj.py
+++ proj.py
@@ -7,6 +7,7 @@ Simulate experiment by Kaufman et al. 2016
 
 from scipy.special import factorial as fact
 import numpy as np
+import itertools
 
 def vec2bin(vec):
     state_str = ''.join(map(str, vec))
@@ -17,20 +18,53 @@ class StateObj:
     def __init__(self, init_vec, idx):
         self.vector = np.asarray(init_vec)
         self.idx = idx
-        
-    @property
-    def  binIndex(self):
-        return vec2bin(self.vector)
     
     prefactor = 1
-    
+
+def creationOp(state, idx, N):
+    a = state.vector[idx]
+    if(a == N):
+        state.prefactor = 0
+    else:
+        state.vector[idx] += 1
+    return state
+
+def annihilationOp(state, idx):
+    a = state.vector[idx]
+    if(a == 0):
+        state.prefactor = 0
+    else:
+        state.vector[idx] -+ 1
+    return state
+
+def numberOp(state, idx):
+    if(state.vector[idx] == 0):
+        state.prefactor = state.prefactor * -1
+    return state
+
 def getBasisStates(N, M):
-    num = fact(N + M - 1)/(fact(N) * fact(M - 1))
-    basis = np.stack([np.zeros(M) for i in range(int(num))])
-    returnDict = {}
     
-    return basis
+    basis, count = [], 0    
+    x = itertools.product(range(N + 1), repeat=M)
+    for i in x:
+        if(np.sum(i) == N):
+            state = StateObj(i, count)
+            basis.append(state)
+            count += 1
+    
+    return np.asarray(basis)
+
+def actHam(state, N, J, U):
+    return_state_list = []
+    # First term
+    for i in range(len(state.vector)-1):
+        return_state_list.append()
+
+def getHamMatrix(basis, J, U):
+    ham_matrix = np.zeros((len(basis), len(basis)))
+    
+    return ham_matrix
     
 if(__name__ == '__main__'):
     print('In module.')
-    print(getBasisStates(2, 2))
+    basis = getBasisStates(2, 3)
